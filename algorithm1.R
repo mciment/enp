a4a0c2c265b92f3da38c52184bc542c210ea279e
mciment <72155457+mciment@users.noreply.github.com>
2020-10-05 17:25:37 +0200

    Update algorithm1.R

--- algorithm1.R
+++ algorithm1.R
@@ -1,3 +1,12 @@
+#**********************************************************************
+#
+# ALGORITHM 1 - POSITIONAL ENTROPY FOR D-ADJACENT.
+#
+# Author: Marcin Cholewa
+# Date created: 27 Julius Anno Currente 2020
+#
+#**********************************************************************
+
 algorithm1<-function(in_sequence, in_d){
 	l<-length(in_sequence)
 	diffs<-0
